f0f989598510e2adbe24726a7952c69d2140735c
santinvalencia <santiamen0020@gmail.com>
2024-06-30 22:40:34 -0300

    enums -
    probablemente esten mal hechos pero que mas da

--- core/src/ENUMS/Direcciones.java
+++ core/src/ENUMS/Direcciones.java
@@ -1,5 +1,15 @@
 package ENUMS;
 
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.Input.Keys;
+
 public enum Direcciones {
-	ARRIBA,IZQUIERDA,DERECHA,ABAJO;
+	ARRIBA(Gdx.input.isKeyPressed(Keys.UP)),IZQUIERDA(Gdx.input.isKeyPressed(Keys.LEFT)),
+	DERECHA(Gdx.input.isKeyPressed(Keys.RIGHT)),ABAJO(Gdx.input.isKeyPressed(Keys.DOWN));
+
+	Direcciones(boolean keyPressed) {
+		// TODO Auto-generated constructor stub
+	}
+
+
 }
