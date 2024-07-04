9a87e4c6708b99e8bab1c99184f53e77571747f4
santinvalencia <santiamen0020@gmail.com>
2024-07-04 19:12:13 -0300

    deltatime

--- core/src/com/coreis/game/MyGdxGame.java
+++ core/src/com/coreis/game/MyGdxGame.java
@@ -3,12 +3,14 @@ package com.coreis.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
+	public static final float SPEED = 120;
 	Texture img;
 	float x;
 	float y;
@@ -16,25 +18,28 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("echeverri.png");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		if(Gdx.input.isKeyPressed(Keys.UP)) {
-			y = y+4;
-		}else if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-			y = y-4;
+			y += +SPEED * Gdx.graphics.getDeltaTime();
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y += -SPEED * Gdx.graphics.getDeltaTime();
 		}
 		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			x = x-4;
-		}else if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			x = x+4;
+			x += -SPEED * Gdx.graphics.getDeltaTime();
+		}
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x += + SPEED * Gdx.graphics.getDeltaTime();
 		}
 		batch.begin();
 		batch.draw(img, x, y);
 		batch.end();
+		
 	}
 	
 	@Override
