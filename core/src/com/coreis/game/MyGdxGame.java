9a1ed1cef2694c600ecc400b4328f486971b1afe
santinvalencia <santiamen0020@gmail.com>
2024-07-04 23:47:21 -0300

    DOS JUGADORES
    meti a jairo y carlitos(echeverri mide 200 pixeles por otros 200 por su escudo en la remera asi que fabri la tienecomplicada)
    - ambos se pueden mover pero eso
    - estoy cansado

--- core/src/com/coreis/game/MyGdxGame.java
+++ core/src/com/coreis/game/MyGdxGame.java
@@ -13,7 +13,10 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	public static final float SPEED = 120;
 	Texture img;
+	Texture img1;
 	float x;
+	float x1= 300;
+	float y1;
 	float y;
 	
 	@Override
@@ -22,8 +25,8 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		this.setScreen(new Splash_Screen());
 		img = new Texture("logoJ.png");
-
-		img = new Texture("echeverri.png");
+		img1 = new Texture("carlitos.png");
+		img = new Texture("jairo.png");
 
 	}
 	private void setScreen(Splash_Screen splash_Screen) {
@@ -33,7 +36,8 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
+		//Jairo
 		if(Gdx.input.isKeyPressed(Keys.UP)) {
 			y += +SPEED * Gdx.graphics.getDeltaTime();
 		}
@@ -46,8 +50,22 @@ public class MyGdxGame extends ApplicationAdapter {
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
 			x += + SPEED * Gdx.graphics.getDeltaTime();
 		}
+		//Carlitos
+		if(Gdx.input.isKeyPressed(Keys.W)) {
+			y1 += +SPEED * Gdx.graphics.getDeltaTime();
+		}
+		if(Gdx.input.isKeyPressed(Keys.S)) {
+			y1 += -SPEED * Gdx.graphics.getDeltaTime();
+		}
+		if(Gdx.input.isKeyPressed(Keys.A)) {
+			x1 += -SPEED * Gdx.graphics.getDeltaTime();
+		}
+		if(Gdx.input.isKeyPressed(Keys.D)) {
+			x1 += + SPEED * Gdx.graphics.getDeltaTime();
+		}
 		batch.begin();
 		batch.draw(img, x, y);
+		batch.draw(img1, x1, y1);
 		batch.end();
 		
 	}
