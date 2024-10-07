c7a613f46a4bd7a48ddb8536f4530180dd91cdf9
santinvalencia <santiamen0020@gmail.com>
2024-10-07 10:23:44 -0300

    asd
    actualziado de png

--- core/src/com/coreis/game/utiles/Recursos.java
+++ core/src/com/coreis/game/utiles/Recursos.java
@@ -1,7 +1,7 @@
 package com.coreis.game.utiles;
 
 public class Recursos {
-	public static final String LOGO = "fondos/logoCVF.PNG";
+	public static final String LOGO = "fondos/logoCVF.png";
 	public static final String MENU = "fondos/MenuScreen.jpeg";
 	public static final String FONDOJUEGO = "fondos/Nocturno.png";
 	public static final String FONDOJUEGO2 = "fondos/Inframundo.png";
